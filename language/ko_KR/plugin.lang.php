780a05205aa12ee5ef11bbd15c3377218a5221c3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-02 14:59:32 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Aspect Ratio'] = '가로세로 비율';
+$lang['Choose the part of the photo you want to keep'] = '사진에서 유지할 부분을 선택하세요.';
+$lang['Crop'] = '자르기';
+$lang['Crop Photo'] = '사진 자르기';
+$lang['Custom'] = '사용자 맞춤';
+$lang['None'] = '없음';
+$lang['Original'] = '원본';
+$lang['Photo Cropped'] = '자른 사진';
+$lang['Square'] = '사각형';
+$lang['Unknown Photo ID'] = '알 수 없는 사진 ID';
